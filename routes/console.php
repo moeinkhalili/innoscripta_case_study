123499946ea4ae6567a0fcfeb36c8c4205942548
moein <moeinkhalili001@gmai.com>
2025-11-26 16:43:29 +0330

    Add throttle

--- routes/console.php
+++ routes/console.php
@@ -3,6 +3,7 @@
 use App\Console\Commands\FetchArticles;
 use Illuminate\Support\Facades\Schedule;
 
+// This should run with cron
 Schedule::command(FetchArticles::class)
     ->everySixHours()
     ->runInBackground();
